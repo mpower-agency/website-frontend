51a009ee9de0c00b7f14a12a7ea7f6e4432b42d4
NagasaiBharat <gs.nsb119@gmail.com>
2022-04-13 22:27:13 +0530

    removed bg, dynamicSize

--- src/components/elements/TestimonialCard.js
+++ src/components/elements/TestimonialCard.js
@@ -8,6 +8,7 @@ const TestimonialCard = (props) => {
   const data = props.data;
   return (
     <Card
+      variant
       sx={{
         display: "flex",
         flexDirection: { xs: "column", lg: "row" },
@@ -15,16 +16,14 @@ const TestimonialCard = (props) => {
         padding: 3,
         marginX: 5,
         marginBottom: 1,
-        boxShadow: 5,
-        bgcolor: "secondary.main",
         borderRadius: 10,
       }}
     >
       <CardMedia
         component="img"
         sx={{
-          width: 120,
-          height: 120,
+          width: { xs: 80, sm: 100, lg: 120 },
+          height: { xs: 80, sm: 100, lg: 120 },
           msTextOverflow: { xs: 5, lg: 0 },
           marginLeft: { md: 3 },
         }}
@@ -33,13 +32,19 @@ const TestimonialCard = (props) => {
       />
       <CardContent
         sx={{
-          marginY: { xs: 0.5 },
+          marginY: { xs: 1 },
           marginLeft: { xs: 1, lg: 5 },
-          padding: { xs: 1, sm: 3, md: 5 },
-          height: { xs: 120, sm: 150, lg: 180 },
+          padding: { xs: 0, sm: 3, md: 5 },
+          minWidth: { xs: 220 },
+          minHeight: { xs: 120, sm: 150, lg: 180 },
         }}
       >
-        <Typography component="h3" variant="text2" sx={{fontWeight:"medium"}} marginBottom={1}>
+        <Typography
+          component="h3"
+          variant="text2"
+          sx={{ fontWeight: "medium" }}
+          marginBottom={1}
+        >
           {data.testimonial}
         </Typography>
         <Typography
@@ -47,7 +52,7 @@ const TestimonialCard = (props) => {
           color="text.secondary"
           component="h6"
           align="left"
-          sx={{fontWeight:"bold"}}
+          sx={{ fontWeight: "bold" }}
         >
           - {data.client}
         </Typography>
