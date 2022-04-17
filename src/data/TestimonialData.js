2ca61777a882cce97972aa7aa20bccac95f2c66f
Ravishankar63 <arunravi3108@gmail.com>
2022-04-17 14:13:21 +0530

    "Testimonials and Social Media Data Changes"

--- src/data/TestimonialData.js
+++ src/data/TestimonialData.js
@@ -2,21 +2,21 @@ const testimonialData = [
   {
     number: 1,
     client: "Will Humphreys, CTO, Primelab (USA)",
-    image: require("../assets/images/companyLogo.png"),
+    image: require("../assets/icons/test_pic_1.png"),
     testimonial:
       "The team was proactive in their service and helped us sail through ups and down in our journey. A long term partnership worthy team. I would highly recommend them for anyone building digital platform.",
   },
   {
     number: 2,
-    client: "Levom Learn Math",
-    image: require("../assets/images/Illustration2.png"),
-    testimonial: "Loved the team",
+    client: "David Hutchkins",
+    image: require("../assets/icons/test_pic_2.png"),
+    testimonial: "The Team was prompt, courteous and acted with great integrity from the start to end of the contract. It was a pleasure discussing matters with them and I would have no hesitation to work with them again in future.",
   },
   {
     number: 3,
-    client: "OtherClient",
-    image: require("../assets/images/Illustration2.png"),
-    testimonial: "He is extremely helpful and knowledgeable.",
+    client: "Karthikeya Reddy",
+    image: require("../assets/icons/test_pic_3.png"),
+    testimonial: "The Team did a good job on NLP task. They were flexible in discussing the work and were able to finish what was asked within the desired time.",
   },
 ];
 export default testimonialData;
